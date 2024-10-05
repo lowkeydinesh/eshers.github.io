95d84fd5450f1e5aca175bd6889f1f31bfd643c4
lowkeydinesh <dinesh.22@ichat.sp.edu.sg>
2024-10-05 22:16:55 +0800

    quiz

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the main cause of recent global warming?",
+        answer: "A. Fossil fuels",
+        options: [
+            "A. Fossil fuels",
+            "B. Deforestation",
+            "C. Agricultural practices",
+            "D. Natural causes"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How much has sea level risen in the past century?",
+        answer: "C. 8 inches",
+        options: [
+            "A. 1 inch",
+            "B. 4 inches",
+            "C. 8 inches",
+            "D. 12 inches"
+        ]
+    },
+    {
+        numb: 3,
+        question: "By what percentage has Arctic ice decreased per decade?",
+        answer: "B. 13%",
+        options: [
+            "A. 5%",
+            "B. 13%",
+            "C. 25%",
+            "D. 50%"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which greenhouse gas has increased the most due to humans?",
+        answer: "B. CO₂",
+        options: [
+            "A. Methane",
+            "B. CO₂",
+            "C. Nitrous Oxide",
+            "D. Ozone"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How much has Earth surface temperature risen since the 1800s?",
+        answer: "B. 1.1°C",
+        options: [
+            "A. 0.5°C",
+            "B. 1.1°C",
+            "C. 1.8°C",
+            "D. 2.5°C"
+        ]
+    },
+];
